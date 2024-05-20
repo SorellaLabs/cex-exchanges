dcb462a3aa5ecc246b1da36953b40c3ef79ae343
jnoorchashm37 <joseph@sorellalabs.xyz>
2024-05-20 17:28:55 -0400

    connections per stream

--- src/exchanges/normalized/types/pairs.rs
+++ src/exchanges/normalized/types/pairs.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 use serde::{Deserialize, Serialize};
 
 use crate::CexExchange;
@@ -95,6 +97,14 @@ impl NormalizedTradingPair {
         } else {
             None
         }
+    }
+}
+
+impl Display for NormalizedTradingPair {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let pair = self.make_pair();
+
+        writeln!(f, "{}", pair)
     }
 }
 
