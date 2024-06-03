c5f6069b7c8f16abf4a63315dfadd1c698d4f1e6
jnoorchashm37 <joseph@sorellalabs.xyz>
2024-06-03 18:00:29 -0500

    added filter

--- src/exchanges/filter.rs
+++ src/exchanges/filter.rs
@@ -14,3 +14,22 @@ impl<T> ExchangeFilter<T> for EmptyFilter {
         true
     }
 }
+
+pub trait IterExchangeFilter<T>: IntoIterator
+where
+    <Self as IntoIterator>::Item: ExchangeFilter<T>
+{
+    fn matches_all(&self, val: &T) -> bool;
+
+    fn matches_any(&self, val: &T) -> bool;
+}
+
+/// this implementation retains values that match ANY of the filters
+impl<T, D> ExchangeFilter<T> for Vec<D>
+where
+    D: ExchangeFilter<T>
+{
+    fn matches(&self, val: &T) -> bool {
+        self.iter().any(|v| v.matches(val))
+    }
+}
